073f8e97478114d0e91c9d34654924a3b87588e3
Artem Tyutnev <artem.tyutnev.developer@gmail.com>
2020-01-20 13:36:36 +0300

    Added constructor

---
+++ unique_ptr.hpp
@@ -0,0 +1,16 @@
+/**
+ * Smart pointer
+ * The allocated memory can belong to only one object.
+ */
+template<typename T>
+class unique_ptr
+{
+private:
+	T* pointer;
+public:
+	unique_ptr(T* pointer) :
+	pointer(pointer)
+	{}
+
+
+};
